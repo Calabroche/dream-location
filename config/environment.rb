be84f0f3a139e8d585389b52db4029f6800d2525
Florian Calabrese florian.calabrese@gmail.com <florian.calabrese@gmail.com>
2023-01-23 15:18:12 +0100

    work on devise

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+
+# ActionMailer::Base.stmp_settings = {
+#   :user_name => 'Florian',
+#   :password => '',
+#   :domain => '',
+#   :address => '',
+#   :port => 587,
+#   :authentication => :plain,
+#   :enable_starttls_auto => true
+# }
