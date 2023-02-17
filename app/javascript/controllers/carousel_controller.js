6041d5ad87fc9623c1ded35b8860c32ac4a7d669
Florian Calabrese florian.calabrese@gmail.com <florian.calabrese@gmail.com>
2023-02-17 11:17:49 +0100

    carousel js

--- app/javascript/controllers/carousel_controller.js
+++ app/javascript/controllers/carousel_controller.js
@@ -1,10 +1,18 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["modal", "image"]
+  static targets = ["modal", "image", "background", "slide"]
+  static values = { index: Number }
+
+
   connect (){
   console.log("coucou");
   console.log(this.imageTarget);
+  console.log(this.modalTarget);
+  console.log(this.backgroundTarget);
+  console.log(this.slideTargets);
+  this.indexValue = 0
+  this.handleKeyDown = this.handleKeyDown.bind(this)
   }
 
   openImage(event) {
@@ -13,9 +21,61 @@ export default class extends Controller {
 
     this.imageTarget.src = imageUrl
     this.modalTarget.style.display = "block"
+    this.backgroundTarget.classList.add('blur')
   }
 
   closeImage() {
     this.modalTarget.style.display = "none"
+    this.backgroundTarget.classList.remove('blur')
+  }
+
+  next() {
+    this.indexValue++
+    if (this.indexValue >= this.slideTargets.length) {
+      this.indexValue = 0
+    }
+  }
+
+  previous() {
+    this.indexValue--
+    if (this.indexValue < 0) {
+      this.indexValue = this.slideTargets.length - 1
+    }
+  }
+
+  get currentIndex() {
+    return this.indexValue
+  }
+
+  set currentIndex(value) {
+    this.indexValue = value
+  }
+
+  handleKeyDown(event) {
+    if (event.keyCode === 37) { // left arrow
+      this.previous()
+    } else if (event.keyCode === 39) { // right arrow
+      this.next()
+    }
+  }
+
+  connect() {
+    document.addEventListener("keydown", this.handleKeyDown)
+  }
+
+  disconnect() {
+    document.removeEventListener("keydown", this.handleKeyDown)
+  }
+
+  update() {
+    this.slideTargets.forEach((slide, index) => {
+      if (index === this.currentIndex) {
+        slide.classList.add("active")
+        slide.classList.remove("inactive")
+      } else {
+        slide.classList.remove("active")
+        slide.classList.add("inactive")
+      }
+    })
   }
 }
