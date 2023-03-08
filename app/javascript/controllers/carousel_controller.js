3e1e0b8feb0d38a14b6eb1d5548f50eabfd1cf62
Florian Calabrese florian.calabrese@gmail.com <florian.calabrese@gmail.com>
2023-03-08 12:12:49 +0100

    work on my carousel show

--- app/javascript/controllers/carousel_controller.js
+++ app/javascript/controllers/carousel_controller.js
@@ -1,31 +1,17 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["modal", "image", "slide"]
-  static values = {
-    index: Number,
-    count: Number
-  }
+  static targets = ["modal", "image"]
+  static values = { index: Number }
 
   connect() {
     console.log(this.imageTarget)
     console.log(this.slideTargets)
     this.index = 0
+    this.indexValue = 0
     this.count = this.slideTargets.length
     this.addKeyboardListener()
-  }
-
-  addKeyboardListener() {
-    document.addEventListener("keydown", (event) => {
-      if (this.modalTarget.style.display === "block") {
-        if (event.key === "ArrowLeft") {
-          console.log("left");
-          this.prevImage()
-        } else if (event.key === "ArrowRight") {
-          this.nextImage()
-        }
-      }
-    })
+    window.aaa = this
   }
 
   openImage(event) {
@@ -40,17 +26,5 @@ export default class extends Controller {
   closeImage() {
     this.modalTarget.classList.remove("opaque-background")
     this.modalTarget.style.display = "none"
-  }
-
-  prevImage() {
-    this.index = (this.index - 1 + this.count) % this.count
-    console.log(this.index);
-    this.imageTarget.src = this.slideTargets[this.index].src
-    console.log(this.imageTarget.src);
-  }
-
-  nextImage() {
-    this.index = (this.index + 1) % this.count
-    this.imageTarget.src = this.slideTargets[this.index].src
   }
 }
