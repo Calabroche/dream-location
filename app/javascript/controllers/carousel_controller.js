83845545f11a767fd65e56674ca61222daad867d
Florian Calabrese florian.calabrese@gmail.com <florian.calabrese@gmail.com>
2023-03-09 17:40:44 +0100

    caroussel stimulus

--- app/javascript/controllers/carousel_controller.js
+++ app/javascript/controllers/carousel_controller.js
@@ -1,16 +1,11 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["modal", "image"]
+  static targets = ["modal", "image", "currentImage", "slide", "imageTwo", "imageThree"]
   static values = { index: Number }
 
   connect() {
     console.log(this.imageTarget)
-    console.log(this.slideTargets)
-    this.index = 0
-    this.indexValue = 0
-    this.count = this.slideTargets.length
-    this.addKeyboardListener()
     window.aaa = this
   }
 
@@ -20,11 +15,35 @@ export default class extends Controller {
     this.imageTarget.src = imageUrl
     this.modalTarget.classList.add("opaque-background")
     this.modalTarget.style.display = "block"
-    this.index = Array.from(this.slideTargets).indexOf(imageElement)
+    this.index = Array.from(this.currentImageTargets).indexOf(imageElement)
+    this.setCurrentImageTarget(imageElement)
+    document.addEventListener("keydown", this.handleKeyDown.bind(this))
   }
 
   closeImage() {
     this.modalTarget.classList.remove("opaque-background")
     this.modalTarget.style.display = "none"
+    document.removeEventListener("keydown", this.handleKeyDown.bind(this))
+  }
+
+  handleKeyDown(event) {
+    if (event.key === "ArrowLeft") {
+      this.index = (this.index - 1 + this.slideTargets.length) % this.slideTargets.length;
+    } else if (event.key === "ArrowRight") {
+      this.index = (this.index + 1) % this.slideTargets.length;
+    }
+    const imageElement = this.slideTargets[this.index].querySelector("img");
+    const imageUrl = imageElement.src;
+    this.imageTarget.src = imageUrl;
+    this.setCurrentImageTarget(imageElement);
+  }
+
+
+
+  setCurrentImageTarget(target) {
+    this.currentImageTargets.forEach((currentImageTarget) => {
+      currentImageTarget.classList.remove("current-image")
+    })
+    target.classList.add("current-image")
   }
 }
